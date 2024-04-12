f988f9269459bd93dc1b8577b38aa6ab0dbb0909
EricSDavis <ericscottdavis@outlook.com>
2024-04-12 18:18:33 -0400

    Correct index; improve button layout for moble and styles;

--- src/components/RoundedButton.jsx
+++ src/components/RoundedButton.jsx
@@ -1,13 +1,11 @@
+// Styles
+import "./RoundedButton.scss"
+
 export default ({text, href}) => {
 
     return (
-        <a href={href} role="button"
-            style={{ 
-                borderRadius: "25px",
-                padding: "5px 20px",
-                marginRight: "15px",
-                marginBottom: "10px"}}>
-                {text}
+        <a href={href} role="button" className="rounded-button">
+            {text}
         </a>
     )
 }
